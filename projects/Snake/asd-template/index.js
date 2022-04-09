380834ea30efc20a27a99aaf39325504d546d1b5
5-matthewkelley <matthewkelley028@gmail.com>
2022-04-09 05:01:52 +0000

    dog

--- projects/Snake/asd-template/index.js
+++ projects/Snake/asd-template/index.js
@@ -13,10 +13,10 @@ function runProgram(){
   
   // Game Item Objects
 
-  $("#Rematch").hide(); // hides the play again button when the code begins so it may be shown later when the game ends
+  $("#Rematch").hide(); // This code makes the button to retry hidden  until the game is over
 
 
-// my factory function used for game object values
+//  The function forthe snake game object values
 function Factory (id, x, y, speedX, speedY) {
   var instance = {
     id: id,
@@ -30,27 +30,27 @@ function Factory (id, x, y, speedX, speedY) {
   return instance;
 };
 
-  var snakeHead = Factory("#snake", 100, 100, 0, 0); // declaration of factory values for the snake's head
-  var snakeBody = [snakeHead];  // array that stores the snake's body segments
-  var apple = Factory("#apple", 100, 200, 0, 0); // declaration of factory values for  the apple
-  var score = 0; // variable that stores the score value
+  var snakeHead = Factory("#snake", 100, 100, 0, 0); // factory values for the snake's head
+  var snakeBody = [snakeHead];  // the code that keeps the snake's body segments
+  var apple = Factory("#apple", 100, 200, 0, 0); // factory values for  the apple
+  var score = 0; // the code that keeps score
 
-  var BOARD_WIDTH = $("#board").width(); // declaration of variable to store board width
-  var BOARD_HEIGHT = $("#board").height(); // declaration of variable to store board height
+  var BOARD_WIDTH = $("#board").width(); // code that tracks board width
+  var BOARD_HEIGHT = $("#board").height(); // code that tracks board height
 
 
-  KEY = {  // key with key-value pairs for the snake's movement keys to have no magic numbers
+  KEY = {  // key-value pairs for the movement of the snake
     "LEFT": 37,
     "UP": 38,
     "RIGHT": 39,
     "DOWN": 40
   };
   // one-time setup
-  var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
+  var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // calls newFrame every 60 Frames per second
   /*
   $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
   */
-  $(document).on('keydown', setSnakespeed); // execute setPlayerspeed() in response to keydown events
+  $(document).on('keydown', setSnakespeed); // calls setPlayerspeed() in response to keydown events
   /*
 	$(document).on('keyup', stopSnakespeed);
   */
@@ -80,17 +80,17 @@ function Factory (id, x, y, speedX, speedY) {
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
 
-  function addSnakeSegment() { // function used to add a new segment to the snake, it is called whenever the snake eats the apple
-    var newID = "snake" + snakeBody.length; // variable that gives the new id of each snake segment
+  function addSnakeSegment() { // code that adds a new segment to the snake when it eats an apple
+    var newID = "snake" + snakeBody.length; // code that gives the new id of each snake segment
 
 
     $("<div>").addClass("snake").attr("id", newID).appendTo("#board"); // code that appends the new snake segment to the board
 
 
-    var tail = snakeBody[snakeBody.length-1]; // array to store the current tail segment
+    var tail = snakeBody[snakeBody.length-1]; // array that keeps the snake at it's length when it ate an apple
 
 
-    var newSegment = Factory("#" + newID, tail.x, tail.y, 0, 0); // code that actually declares the new segment
+    var newSegment = Factory("#" + newID, tail.x, tail.y, 0, 0); // code that calls the new segment
     /*
     redrawGameItem(newSegment);
     */
@@ -98,7 +98,7 @@ function Factory (id, x, y, speedX, speedY) {
     snakeBody.push(newSegment); // pushes the new segment into the body
   }
 
-  function repositionBody() { // repositions the body segments to follow and flow with the head like in Snake
+  function repositionBody() { // repositions the body segments to follow with the head like in Snake
     for(var i = snakeBody.length-1;i >= 1;i--){
       snakeBody[i].x = snakeBody[i-1].x;
       snakeBody[i].y = snakeBody[i-1].y;
@@ -107,12 +107,11 @@ function Factory (id, x, y, speedX, speedY) {
     }
   }
 
-  function endGame() { // called when game ends (you die by hitting yourself or borders)
+  function endGame() { // code that tells you when the game is over and ask for a rematch
     clearInterval(interval);
 		$(document).off();
-		$('#snakeEndScreen').text("Game Over"); // writes text on screen that says 'Game Over'
-		$("#Rematch").show(); // shows button that you can click to play again
-		$("#Rematch").on("click", Rematch); // makes the button register clicks in order to play again
+		$("#Rematch").show(); // code for a button that you can click to play Snake again
+		$("#Rematch").on("click", Rematch); // makes the button register clicks in order to play Snake again
   }
 
   // reloads the game by reloading the webpage
@@ -120,7 +119,7 @@ function Factory (id, x, y, speedX, speedY) {
 		location.reload();
 	}
 
-  function repositionSnake(obj) { // function used to reposition the snake across the board with its speed (basically make it move)
+  function repositionSnake(obj) { // The code that makes the snake move
     obj.x += obj.speedX;
     obj.y += obj.speedY;
     /*
@@ -133,11 +132,11 @@ function Factory (id, x, y, speedX, speedY) {
     $(obj.id).css("top", obj.y);
   }
   
-  function generateApple() { // function that is called whenever apple is eaten, generates a new apple in any random tile within the board that isn't occupied by snake
+  function generateApple() { // function that is called whenever apple is eaten, generates a new apple in a random place where the snake isn't there either
     apple.x = Math.floor(Math.random() * 25) * 20;
     apple.y = Math.floor(Math.random() * 25) * 20;
     
-    for(var i = snakeBody.length-1;i >= 1;i--) { // checks if the newly generated apple from the earlier code in the function actually appears over the snake, if it does it simply re-executes the function until it doesn't appear over the snake anymore
+    for(var i = snakeBody.length-1;i >= 1;i--) { // checks if the newly appeared apple from the last one code didn't go on the snake
       if (snakeHead.y === apple.y && snakeHead.x === apple.x || snakeBody[i].y === apple.y && snakeBody[i].x === apple.x) {
         generateApple();
       }
